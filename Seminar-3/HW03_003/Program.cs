50097a5e8793cee73c3798681130e43c46c905a3
altotass <altotass@gmail.com>
2022-08-30 18:45:42 +0300

    HW03_003

---
+++ Seminar-3/HW03_003/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+//void Quadron()
+//{
+        Console.WriteLine("Введите число N = ");
+        int N = int.Parse(Console.ReadLine());
+        double x;
+        for (int i = 1; i <= N; i++)
+        {
+            x = Math.Pow(i,3);
+            Console.Write($"{x}, ");
+        }
+    try
+    {
+    //Quadron();
+    }
+        catch
+        {
+        Console.WriteLine("Введите положительное число от 1 ...");
+        }
+//}
