9d80716cf855c431c54e30abd09407bf2fa56cf8
altotass <altotass@gmail.com>
2022-08-30 15:47:57 +0300

    HW03_001

---
+++ Seminar-3/HW03_001/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+void Palindrom()
+{
+Console.WriteLine("Введите пятизначное число, х = ");
+            string s = Console.ReadLine();
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = s.Length - 1; j > 0; j--)
+                {
+                    if (s[i] == s[j])                         
+                    Console.WriteLine("Введенная строка палиндром");
+                    else                         
+                    Console.WriteLine("Введенная строка не палиндром");
+                return;
+                }
+            }
+}
+Palindrom();
+ 
